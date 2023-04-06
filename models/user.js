74cbf65b3cb619b46e66e496dfa0174f1f327afd
youjin <w2117@e-mirim.hs.kr>
2023-04-06 20:04:35 +0900

    server

---
+++ models/user.js
@@ -0,0 +1,18 @@
+// 설치 : npm install mongoose --save
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
