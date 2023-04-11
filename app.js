d1c1453094b3455debc3e542aec50e0955eaf763
youjin <w2117@e-mirim.hs.kr>
2023-04-11 20:03:12 +0900

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+// JSON과 url-encoded 데이터를 파싱할 수 있도록 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 세션 설정
+app.use(session({
+    secret: 'secret', // 세션 ID를 암호화할 때 사용하는 키
+    resave: false, // 변경되지 않은 세션은 저장하지 않도록 설정
+    saveUninitialized: false // 초기화되지 않은 세션은 저장하지 않도록 설정
+}));
+
+// Passport 초기화 및 세션 사용 설정
+app.use(passport.initialize());
+app.use(passport.session());
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://w2117:hki050411@cluster0.okgzgum.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
